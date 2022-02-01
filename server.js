30621078074cc3ef5597dfe11dca17faa7c06ea1
viktor <vpodhornyi@gmail.com>
2022-02-01 19:03:19 +0200

    add .env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 (() => {
   try {
     app.listen(PORT, () => console.log(`server run oh port - ${PORT}`));

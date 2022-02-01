d9ef7551de68d49589d39f71397e3f90089272d2
viktor <vpodhornyi@gmail.com>
2022-02-01 19:06:13 +0200

    add .env incorrect res status

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.get('/ping', (req, res) => {
-  res.status(200).end('pong');
+  res.status(201).end('pong');
 })
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 (() => {
   try {
-    app.listen(PORT, () => console.log(`server run oh port - ${PORT}`));
+    app.listen(PORT, () => console.log(`server run on port - ${PORT}`));
   } catch (err) {
     console.log(err);
   }

ac4d1195a47233e66fa188d61ff48ec020a1894e
viktor <vpodhornyi@gmail.com>
2022-02-05 02:10:54 +0200

    feature/server: test fix

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -30,6 +33,10 @@ class App {
 
   get models() {
     return this.#_models;
+  }
+
+  get app() {
+    return this.#_app;
   }
 
   start() {
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
 import App from './App.js';
 
 new App().start();

d496dc00d305a652863c45fb33c6d63869e640cc
viktor <vpodhornyi@gmail.com>
2022-02-09 00:21:21 +0200

    feature/server: ref get models to Class

---
+++ src/models/Model.js
@@ -0,0 +1,26 @@
+import getUser from './User.js';
+import getTweet from './Tweet.js';
+
+class Model {
+  #_models;
+
+  constructor(sequelize) {
+    this.#_models = {
+      User: getUser(sequelize),
+      Tweet: getTweet(sequelize),
+    }
+    this.#associateUserTweet();
+  }
+
+  #associateUserTweet = () => {
+    const {User, Tweet} = this.#_models;
+    Tweet.belongsTo(User);
+    User.hasMany(Tweet);
+  }
+
+  get models() {
+    return this.#_models;
+  }
+}
+
+export default Model;
